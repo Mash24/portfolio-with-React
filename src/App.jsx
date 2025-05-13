8423cfca636ae7e4475b87df1ce1c6c6abf23ac5
mash <urexz123@gmail.com>
2025-05-13 12:45:40 +0700

    Added yaml file for AWS CodeBuild pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BlogDetail from './components/blog/BlogDetail';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <main className="font-sans">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog/:slug" element={<BlogDetail />} />
-      </Routes>
+      <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog/:slug" element={<BlogDetail />} />
+        </Routes>
+        <SpeedInsights />
+      </>
     </main>
   );
 }
